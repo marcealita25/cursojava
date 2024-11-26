29e066eaa343164ef9cb29efafe527937fc0878f
Tardes <Tardes@GET-PROF>
2024-11-26 17:49:19 +0100

    Nuevo cambio

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	public static void main (String[] args) {
 		//Comentario de línea
 		
-		System.out.println("Hola Mundo2");
+		System.out.println("Hola Mundo3");
 		/*
 		 * Comentario línea 1
 		 * Comentario línea 2
