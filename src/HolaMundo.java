12d83fdc83b18ab971aff0a44f770a430731cdcf
Tardes <Tardes@GET-PROF>
2024-11-26 17:41:58 +0100

    Primera versión del proyecto

---
+++ src/HolaMundo.java
@@ -0,0 +1,15 @@
+
+public class HolaMundo {
+	public static void main (String[] args) {
+		//Comentario de línea
+		
+		System.out.println("Hola Mundo");
+		/*
+		 * Comentario línea 1
+		 * Comentario línea 2
+		 * Comentario línea 3
+		 * 
+		 */
+		
+	}
+}
