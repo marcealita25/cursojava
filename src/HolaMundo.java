3bc2bdb0b1a0e43a01da2cc365fb992e2ec7ba5b
jlllorente6 <joseluis.llorenteperales+6@gmail.com>
2024-11-27 18:03:18 +0100

    Update desde GitHub

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	public static void main (String[] args) {
 		//Comentario de línea
 		
-		System.out.println("Hola Mundo5");
+		System.out.println("Hola Mundo6");
 		/*
 		 * Comentario línea 1
 		 * Comentario línea 2
